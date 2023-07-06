bc307d24c29d1044b7ee82ed38f71dbe935112fb
Eiker <eiker.gonzalez21>
2023-07-06 18:22:09 -0400

    ajuste smn_plantilla_movimiento_bancario

--- WEB-INF/action/smn_proceso/smn_plantilla_movimiento_bancario/filter/getsucursal/sucursales.js
+++ WEB-INF/action/smn_proceso/smn_plantilla_movimiento_bancario/filter/getsucursal/sucursales.js
@@ -2,7 +2,7 @@ listboxClear("smn_sucursal_rf");
 
 var optionChoose = document.createElement("option");
 optionChoose.text = '[${lbl:b_choose}]';
-optionChoose.value = "0";
+optionChoose.value = "";
 document.form1.smn_sucursal_rf.add(optionChoose, 0);
 
 <smn_sucursal_rf_rows>
--- WEB-INF/action/smn_proceso/smn_plantilla_movimiento_bancario/filter/smn_plantilla_movimiento_bancario_det/repgen/search/clause-pmd_numero_referencia_bancaria.sql
+++
@@ -1,2 +0,0 @@
- and
- 	smn_banco.smn_plantilla_movimiento_bancario_det.pmd_numero_referencia_bancaria=${fld:pmd_numero_referencia_bancaria}
